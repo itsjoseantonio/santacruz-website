d5865d24b13be3951a48cc95dcd4cf08444e221e
Jose Antonio <jos.valga@gmail.com>
2024-07-14 21:01:00 -0500

    feat: added mobile menu

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,3 +1,12 @@
 $('#myCarousel').on('click', () => {
-  console.log('jQuery is working');
+    console.log('jQuery is working');
+});
+
+// JavaScript Version
+const buttonMenu = document.querySelector('.hamburguer');
+const navMenu = document.querySelector('.navbar-ul');
+
+buttonMenu.addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('active');
+    navMenu.classList.toggle('active');
 });
