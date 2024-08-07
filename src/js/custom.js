4c6f744ee5b0f8b5fc8331a668e6223246fb4a5c
Jose Antonio <jos.valga@gmail.com>
2024-08-06 21:02:13 -0500

    fix: fixed menu mobile

--- src/js/custom.js
+++ src/js/custom.js
@@ -6,8 +6,10 @@ $('#myCarousel').on('click', () => {
 const buttonMenu = document.querySelector('.hamburguer');
 const navMenu = document.querySelector('.navbar-ul');
 const nav = document.querySelector('.navbar');
+const body = document.querySelector('.body');
 
 buttonMenu.addEventListener('click', (e) => {
     e.currentTarget.classList.toggle('active');
     navMenu.classList.toggle('active');
+    body.classList.toggle('fixed');
 });
